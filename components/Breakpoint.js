bdc318622078c826652ea3a0ca744f30397403ee
patrykkopycinski <contact@patrykkopycinski.com>
2016-04-19 06:11:43 +0200

    Add support for react 15.0.1 (#7)

--- components/Breakpoint.js
+++ components/Breakpoint.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
-require('./breakpoints.css')
+import './breakpoints.css'
 
 class Breakpoint extends Component {
   render () {
     const { mobile, children } = this.props
+
     if (mobile) {
       return (
         <div className="breakpoint-min-width-700">
           {children}
         </div>
       )
-    } else {
-      return (
-        <div className="breakpoint-max-width-700">
-          {children}
-        </div>
-      )
     }
+
+    return (
+      <div className="breakpoint-max-width-700">
+        {children}
+      </div>
+    )
   }
 }
 
 Breakpoint.propTypes = {
-  children: React.PropTypes.object,
+  children: React.PropTypes.array,
   mobile: React.PropTypes.bool,
 }
 
